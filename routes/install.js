d00a070b50c691d0a051db055fe2c24d83122560
Karthik Bhat <karthik.ax.ax@gmail.com>
2016-02-10 05:09:46 +0530

    Install View

--- routes/install.js
+++ routes/install.js
@@ -4,9 +4,28 @@ var router = express.Router();
 /* GET install main. */
 router.get('/', function(req, res, next) {
 	res.render('install', {
+		currStep: 1,
 		title: 'Install',
 		languages: { "en": "english", "hi": "hindi" }
 	});
+});
+
+router.post('/', function(req, res) {
+	if(req.body.currStep == "1"){
+		// set default language req.body.language
+		res.render('install', {
+			currStep: 2,
+			title: 'Admin Details',
+			url: req.get('host')+'/'
+		});
+	}
+	if(req.body.currStep == "2"){
+		// Add documents to collections
+		res.render('install', {
+			currStep: 3,
+			title: 'Completed'
+		});
+	}
 });
 
 module.exports = router;
